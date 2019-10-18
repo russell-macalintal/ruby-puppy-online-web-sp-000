141f1ca2d77d961f494eb9b6bde9058c0b1e5dfa
Russell Macalintal <russell.macalintal@gmail.com>
2019-10-18 02:30:30 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
 # Add your code here
+class Dog
+  @@all = []  #Class variable
+  attr_reader :name  #Instance Variable
+
+  def initialize(name)
+    @name = name
+    self.save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.print_all
+    self.all.each do |dog|
+      puts dog.name
+    end
+  end
+
+  def self.clear_all
+    @@all = []
+  end
+
+  def save
+    @@all << self
+  end
+end
